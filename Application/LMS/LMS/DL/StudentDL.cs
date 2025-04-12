3920cb502485909e49dad34da632c8bf07336e03
saubanahmad <saubanahmad05@gmail.com>
2025-04-12 14:13:28 +0500

    first commit

---
+++ Application/LMS/LMS/DL/StudentDL.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using LMS.BL;
+
+namespace LMS.DL
+{
+    internal class StudentDL
+    {
+        public static List<Student> students = new List<Student>();
+
+        public static void AddStudent(Student s)
+        {
+            string query = $"INSERT INTO students VALUES  ('{s.Student_id}', '{s.Student_name}', '{s.Age}', '{s.Email}', '{s.Phone}', '{s.CNIC}', '{s.Guardian_name}', '{s.Guardian_phone}', '{s.Generated_password}')";
+            DatabaseHelper.Instance.Update(query);
+        }
+
+
+        //public static void UpdateStudent()
+        // make customized updation i.e.student can update any particular attribute 
+    }
+}
